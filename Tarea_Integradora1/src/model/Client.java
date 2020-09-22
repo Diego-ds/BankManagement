ca684d8a6284889108b2668275c3ccd203cfd55f
DSMontoyaP <55026204+DSMontoyaP@users.noreply.github.com>
2020-09-22 14:40:44 -0500

    Updated classes

--- Tarea_Integradora1/src/model/Client.java
+++ Tarea_Integradora1/src/model/Client.java
@@ -3,5 +3,6 @@ package model;
 public class Client {
 
 	private String name;
+	private String accountNumber;
 	
 }
--- Tarea_Integradora1/src/ui/MainBank.java
+++ Tarea_Integradora1/src/ui/MainBank.java
@@ -5,6 +5,7 @@ public class MainBank {
 	public static void main(String[] args) {
 		System.out.print("AAAAAA");
 		System.out.println("prueba git");
+
 	}
 
 }
